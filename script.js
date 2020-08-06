749d0c3d2aa98fac5404f0adec7eb94ab5612df1
Anna Brandenberger <anna.brandenberger@mail.mcgill.ca>
2020-08-06 10:31:29 -0400

    edit style and add title and filler text

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function initializeMap(root, x, y, w) {
     }
 }
 
-console.log('tester tree height', testert.height, 'size', testert.size, 'left subtree height', testert.children[0].height, 'size', testert.children[0].size)
+// console.log('tester tree height', testert.height, 'size', testert.size, 'left subtree height', testert.children[0].height, 'size', testert.children[0].size)
 
 function newTree() {
     t = Tree.randomTree('manual', [8, 12, 6, 1]);
@@ -90,30 +90,27 @@ function toggleFullScreen() {
         resizeCanvas(window.innerWidth, window.innerHeight);
         resizeButton.html('<i class = "fa fa-compress"> </i>');
     } else {
-        resizeCanvas(window.innerWidth * 3 / 4, window.innerHeight * 3 / 4);
+        resizeCanvas(Math.floor(window.innerWidth * 3 / 4), Math.floor(window.innerHeight * 3 / 4));
         resizeButton.html('<i class = "fa fa-expand"> </i>');
     }
     isFullScreen = !isFullScreen;
-    repositionButtons();
+    document.body.classList.toggle('fullscreen');
 }
 
 function windowResized() { // p5: runs when window is resized
     if (isFullScreen) {
         resizeCanvas(window.innerWidth, window.innerHeight);
     } else {
-        resizeCanvas(window.innerWidth * 3 / 4, window.innerHeight * 3 / 4)
+        resizeCanvas(Math.floor(window.innerWidth * 3 / 4), Math.floor(window.innerHeight * 3 / 4));
     }
-    repositionButtons();
 }
 
-function repositionButtons() {
-    let pos = canvas.position();
-    let buttonSize = resizeButton.size();
-    resizeButton.position(pos.x + width - buttonSize.width, pos.y + height - buttonSize.height);
-    print(buttonSize.width, buttonSize.height);
-
-    treeButton.position(pos.x, pos.y);
-}
+// function repositionButtons() {
+//     let pos = canvas.position();
+//     let buttonSize = resizeButton.size();
+//     resizeButton.position(pos.x + width - buttonSize.width, pos.y + height - buttonSize.height);
+//     treeButton.position(pos.x, pos.y);
+// }
 
 function drawTree(root, x, y, w) {
     if (root.children.length == 0) {
@@ -165,7 +162,8 @@ function drawTreeFromMap(root, positions, scalings) {
 }
 
 function setup() {
-    canvas = createCanvas(window.innerWidth * 3 / 4, window.innerHeight * 3 / 4);
+    canvas = createCanvas(Math.floor(window.innerWidth * 3 / 4), Math.floor(window.innerHeight * 3 / 4));
+    canvas.parent('canvas');
     stroke(0, 125);
     levelDiff = (height - 20) / h;
     textSize(8);
@@ -174,56 +172,68 @@ function setup() {
     resizeButton = createButton('Full Screen');
     resizeButton.html('<i class = "fa fa-expand"> </i>');
     resizeButton.mouseClicked(toggleFullScreen);
+    resizeButton.addClass('bottomright'); 
+    resizeButton.parent('canvas');
 
     treeButton = createButton('New Tree');
-        treeButton.mouseClicked(newTree);
+    treeButton.mouseClicked(newTree);
+    
+    treeButton.addClass('topleft');
+    treeButton.parent('canvas');
+}
 
-        repositionButtons();
-    }
-
-    function draw() {
-        background(240);
-        let scaleValue = min(width, height);
-        let scalings = {
-                minX: Infinity,
-                minY: Infinity,
-                maxX: -Infinity,
-                maxY: -Infinity
-            }
-            // update accelerations then velocities then positions
-        positions.forEach((p, nodep) => {
-            p.ax = 0;
-            p.ay = 0;
-            positions.forEach((q, nodeq) => {
-                if (p != q) {
-                    let d = dist(p.x, p.y, q.x, q.y);
-                    let sx = (q.x - p.x) / d; // unit vector
-                    let sy = (q.y - p.y) / d;
-                    // repelling force between all nodes 
-                    p.ax += -g * sx / (d + 1) ** 2;
-                    p.ay += -g * sy / (d + 1) ** 2;
-                    if (t.areNeighbours(nodep, nodeq)) {
-                        // attractive force 
-                        // p.ax += g * sx / (d + 1) ** 2;
-                        // p.ay += g * sy / (d + 1) ** 2;
-                        // spring force 
-                        p.ax += k * sx * (d - 100);
-                        p.ay += k * sy * (d - 100);
-                    }
+function draw() {
+    background(240);
+    let scaleValue = min(width, height);
+    let scalings = {
+            minX: Infinity,
+            minY: Infinity,
+            maxX: -Infinity,
+            maxY: -Infinity
+        }
+        // update accelerations then velocities then positions
+    positions.forEach((p, nodep) => {
+        p.ax = 0;
+        p.ay = 0;
+        positions.forEach((q, nodeq) => {
+            if (p != q) {
+                let d = dist(p.x, p.y, q.x, q.y);
+                let sx = (q.x - p.x) / d; // unit vector
+                let sy = (q.y - p.y) / d;
+                // repelling force between all nodes 
+                p.ax += -g * sx / (d + 1) ** 2;
+                p.ay += -g * sy / (d + 1) ** 2;
+                if (t.areNeighbours(nodep, nodeq)) {
+                    // attractive force 
+                    // p.ax += g * sx / (d + 1) ** 2;
+                    // p.ay += g * sy / (d + 1) ** 2;
+                    // spring force 
+                    p.ax += k * sx * (d - 100);
+                    p.ay += k * sy * (d - 100);
                 }
-            })
+            }
         })
+    })
 
-        positions.forEach((p, node) => {
-            p.vx += p.ax - gamma * p.vx;
-            p.vy += p.ay - gamma * p.vy;
-            p.x += p.vx;
-            p.y += p.vy;
-            if (p.x <= scalings.minX) { scalings.minX = p.x; }
-            if (p.x >= scalings.maxX) { scalings.maxX = p.x; }
-            if (p.y <= scalings.minY) { scalings.minY = p.y; }
-            if (p.y >= scalings.maxY) { scalings.maxY = p.y; }
-        })
+    positions.forEach((p, node) => {
+        p.vx += p.ax - gamma * p.vx;
+        p.vy += p.ay - gamma * p.vy;
+        p.x += p.vx;
+        p.y += p.vy;
+        if (p.x <= scalings.minX) { scalings.minX = p.x; }
+        if (p.x >= scalings.maxX) { scalings.maxX = p.x; }
+        if (p.y <= scalings.minY) { scalings.minY = p.y; }
+        if (p.y >= scalings.maxY) { scalings.maxY = p.y; }
+    })
 
-        drawTreeFromMap(t, positions, scalings);
-    }
+    drawTreeFromMap(t, positions, scalings);
+}
+
+// window stuff
+window.onload = () => {
+    fetch('description.txt')
+        .then(res => res.text())
+        .then(s => {
+            document.getElementById('description').innerHTML = s;
+        });
+}
