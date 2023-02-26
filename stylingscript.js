73c783c9a2e70a5f72bd88189f4c05522c658c43
Anna Brandenberger <anna.brandenberger@mail.mcgill.ca>
2023-02-26 18:15:05 -0500

    fix marked bug

--- stylingscript.js
+++ stylingscript.js
@@ -1,44 +1,48 @@
 function timedvisibility(thing) {
-    setTimeout(function() {
-        thing.classList.toggle('notvisible');
-    }, 500);
+  setTimeout(function () {
+    thing.classList.toggle("notvisible");
+  }, 500);
 }
 
 function toggle() {
-    let help = document.getElementById("helptext");
-    help.classList.toggle('openheight');
-    let helpbutton = document.getElementById("helpbutton");
-    helpbutton.classList.toggle('buttonon');
-    let helptext = document.getElementById("helptextcontent");
-    if (helptext.classList.contains('notvisible')) {
-        timedvisibility(helptext);
-    }
-    else {
-        helptext.classList.toggle('notvisible');
-    }
+  let help = document.getElementById("helptext");
+  help.classList.toggle("openheight");
+  let helpbutton = document.getElementById("helpbutton");
+  helpbutton.classList.toggle("buttonon");
+  let helptext = document.getElementById("helptextcontent");
+  if (helptext.classList.contains("notvisible")) {
+    timedvisibility(helptext);
+  } else {
+    helptext.classList.toggle("notvisible");
+  }
 }
 
 function resizeUnderCanvas() {
-    let innerHeight = document.getElementById('radiodiv').clientHeight;
-    document.getElementById('undercanvas').setAttribute("style","min-height:"+innerHeight.toString()+'px');
+  let innerHeight = document.getElementById("radiodiv").clientHeight;
+  document
+    .getElementById("undercanvas")
+    .setAttribute("style", "min-height:" + innerHeight.toString() + "px");
 }
 
 // window stuff
 window.onload = () => {
-    fetch('description.md')
-        .then(res => res.text())
-        .then(s => {
-            document.getElementById('description').innerHTML = marked.parse(s);
-            renderMathInElement(document.body, options = {
-                delimeters: [
-                    { left: "$$", right: "$$", display: true },
-                    { left: "$", right: "$", display: false },
-                    { left: "\\(", right: "\\)", display: false },
-                    { left: "\\[", right: "\\]", display: true }
-                ]
-            });
-            resizeUnderCanvas();
-        });
-    spinner = document.getElementById('spinnericon');
-}
-window.addEventListener('resize', resizeUnderCanvas, true);
+  fetch("description.md")
+    .then((res) => res.text())
+    .then((s) => {
+      document.getElementById("description").innerHTML = marked.parse(s);
+      renderMathInElement(
+        document.body,
+        (options = {
+          delimeters: [
+            { left: "$$", right: "$$", display: true },
+            { left: "$", right: "$", display: false },
+            { left: "\\(", right: "\\)", display: false },
+            { left: "\\[", right: "\\]", display: true },
+          ],
+        })
+      );
+      resizeUnderCanvas();
+    });
+  spinner = document.getElementById("spinnericon");
+};
+window.addEventListener("resize", resizeUnderCanvas, true);
